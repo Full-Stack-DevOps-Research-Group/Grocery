ff0e94a7d985d13f6ca20e361ba6aa87e504e331
kiranlimbu <mrlimbu@gmail.com>
2020-11-13 15:37:42 -0800

    Grocery API Test

---
+++ GroceryAPI/Controllers/GroceryController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GroceryAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace GroceryAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GroceryController : ControllerBase
+    {
+        private ILogger<GroceryController> _logger;
+        public GroceryController(ILogger<GroceryController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public GroceryItem Get()
+        {
+            return new GroceryItem
+            {
+                Name = "value1",
+                Price = 2
+            };
+        }
+
+        [HttpPost]
+        public GroceryItem Post(GroceryItem item)
+        {
+            return item;
+        }
+    }
+}
